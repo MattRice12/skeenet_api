a8d7f429fef163861837758a7c989cf86b980024
Matt Rice <mattrice12@outlook.com>
2018-06-23 15:20:34 -0500

    added team score total for game

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def frame_scores(frame, player, game, team)
+    score = Score.find_by(frame_id: frame.id, player_id: player.id, game_id: game.id, team_id: team.id)
+    return 0 if score.nil?
+    score.points
+  end
+
+  def player_score(player, game, team)
+    scores = Score.where(player_id: player.id, game_id: game.id, team_id: team.id)
+    return 0 if scores.blank?
+    scores.pluck(:points).sum
+  end
 end
